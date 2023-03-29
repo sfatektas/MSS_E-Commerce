1b64846096b3e2da578606023ed3a9374b82fea2
sfatektas <sfatektas55@hotmail.com>
2023-03-29 17:56:19 +0300

    context added

---
+++ E-Commerce.DataAccess/Contexts/E_CommerceDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.DataAccess.Contexts
+{
+    internal class E_CommerceDbContext
+    {
+    }
+}
