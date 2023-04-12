fb4402a110d731fe1455ce896b2430fa42b0f5a0
selimgunaydin <sselimgunaydin@gmail.com>
2023-04-12 18:29:49 +0300

    Font düzenlendi. Kategori denetimi yapıldı.

--- E-Commerce.React/src/pages/Category.jsx
+++ E-Commerce.React/src/pages/Category.jsx
@@ -1,12 +1,29 @@
 import { useParams } from "react-router-dom";
+import { generalStore } from "../store/generalStore";
+import { Navigate } from "react-router-dom";
+import { isValid } from "js-base64";
 
 export default function Category(props) {
-  const {defination} = useParams();
-  return (
-    <>
-      <div className="container vh-100">
-        <p className="my-4 ">HOME | Category | <span className="fw-bold">{defination}</span></p>
-      </div>
-    </>
-  );
+  const { defination } = useParams();
+  const { categories } = generalStore();
+
+  try {
+    let definationList = categories.map((item) => {
+      return item.defination;
+    });
+    let isValid = definationList.includes(defination);
+    if (isValid) {
+      return (
+        <>
+          <div className="container vh-100">
+            <p className="my-4 ">
+              HOME | Category | <span className="fw-bold">{defination}</span>
+            </p>
+          </div>
+        </>
+      );
+    } else {
+      return <Navigate to="/NotFound" />;
+    }
+  } catch {}
 }
