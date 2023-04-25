ff0e8ec729ca7eaf5c03e012133eb81b0ec5b230
selimgunaydin <sselimgunaydin@gmail.com>
2023-04-25 19:32:26 +0300

    Build dosyaları eklendi.

--- E-Commerce.React/src/components/home/HomeProducts.jsx
+++ E-Commerce.React/src/components/home/HomeProducts.jsx
@@ -2,46 +2,45 @@ import { useEffect, useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import productJson from "../../products.json";
 import Showcase from "../common/Showcase";
+import axios from "axios";
 
 export default function HomeProducts() {
-  // const [homeProducts, sethomeProducts] = useState([]);
+  const [homeProducts, setHomeProducts] = useState([]);
 
-  // useEffect(() => {
-  //   sethomeProducts(data);
-  // }, [homeProducts]);
-
-  // useEffect(() => {
-  //   function shuffleProducts(array) {
-  //     for (let i = array.length - 1; i > 0; i--) {
-  //       const j = Math.floor(Math.random() * (i + 1));
-  //       [array[i], array[j]] = [array[j], array[i]];
-  //     }
-  //     return array;
-  //   }
-  //   shuffleProducts(data);
-  // }, [data]);
-
+  useEffect(() => {
+    axios
+      .get("https://e-commercemss.azurewebsites.net/api/SalesProducts/home")
+      .then((response) => {
+        setHomeProducts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [homeProducts]);
 
   return (
     <div className="products-main container mb-5">
-      {/* <div className="products-header d-flex flex-column align-items-center mb-4">
+      <div className="products-header d-flex flex-column align-items-center mb-4">
         <span className="title display-6 fw-bold mb-3">KARIŞIK ÜRÜNLER</span>
         <p className="text-muted">Premium satıcılarımızın ürünleri</p>
       </div>
       <div className="products-content">
         <div className="row">
-          {homeProducts.slice(0, 12).map((item) => (
-            <Showcase
-              key={item.id}
-              id={item.id}
-              title={item.title}
-              category={item.category}
-              price={item.price}
-              image={item.image}
-            />
-          ))}
+          {homeProducts.slice(0, 8).map((item) => {
+            return (
+              <Showcase
+                key={item.supplierProductId}
+                id={item.supplierProductId}
+                title={item.productTitle}
+                brand={item.brand.defination}
+                price={item.unitPrice}
+                image={item.imageUrls[0]}
+                category={item.category.defination}
+              />
+            );
+          })}
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
