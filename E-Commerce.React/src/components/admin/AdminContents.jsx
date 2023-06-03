337667a82ede080bad4c926882630d170f3da331
selimgunaydin <sselimgunaydin@gmail.com>
2023-06-03 18:01:21 +0300

    API Güncellemeleri ve Axios Method Dinamikleştirme Gereksiz kod temizliği.

--- E-Commerce.React/src/components/admin/AdminContents.jsx
+++ E-Commerce.React/src/components/admin/AdminContents.jsx
@@ -14,6 +14,23 @@ export default function AdminContents() {
   const [twoBanner, setTwoBanner] = useState([]);
   const [threeBanner, setThreeBanner] = useState([]);
   const [management, setManagement] = useState("carousel");
+  const [sliderChangeModal, setSliderChangeModal] = useState();
+  const [changeSliderItemId, setChangeSliderItemId] = useState();
+
+  function updateData() {
+    axios
+      .get(`http://api.mssdev.online/api/Slider`)
+      .then((response) => {
+        setCarousel(response.data[0]);
+        setTwoBanner(response.data[1]);
+        setThreeBanner(response.data[2]);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   useEffect(() => {
     axios
       .get(`http://api.mssdev.online/api/Slider`)
@@ -21,7 +38,7 @@ export default function AdminContents() {
         setCarousel(response.data[0]);
         setTwoBanner(response.data[1]);
         setThreeBanner(response.data[2]);
-        console.log(response.data[0]);
+        console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -40,102 +57,179 @@ export default function AdminContents() {
           setInfo("Slider Item başarıyla silindi");
           setVariant("success");
           setInfoModal(true);
+          updateData();
         })
         .catch((error) => {
           console.log(error.response);
         });
     }
   }
-  function TwoBanner() {
+
+  function ChangeSliderItemModal(props) {
+    const [editableSlider, setEditableSlider] = useState([]);
+    const [editableSliderId, setEditableSliderId] = useState();
+    const [editableSliderItemId, setEditableSliderItemId] = useState();
+    const [editableSliderTitle, setEditableSliderTitle] = useState();
+    const [editableSliderSubtitle, setEditableSliderSubtitle] = useState();
+    const [editableSliderButtonText, setEditableSliderButtonText] = useState();
+    const [editableSliderButtonLink, setEditableSliderButtonLink] = useState();
+    const [editableSliderFile, setEditableSliderFile] = useState();
+    useEffect(() => {
+      axios
+        .get(`http://api.mssdev.online/api/SliderItem/${changeSliderItemId}`)
+        .then((response) => {
+          setEditableSlider(response.data);
+          setEditableSliderId(response.data.sliderId);
+          setEditableSliderItemId(response.data.id);
+          setEditableSliderTitle(response.data.title);
+          setEditableSliderSubtitle(response.data.subTitle);
+          setEditableSliderButtonText(response.data.buttonText);
+          setEditableSliderButtonLink(response.data.buttonLink);
+          setEditableSliderFile(response.data.imageUrl);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, []);
+    function handleFile(event) {
+      event.preventDefault();
+      setEditableSliderFile(event.target.files);
+    }
+
+    function handleSubmit(event) {
+      event.preventDefault();
+
+      const formData = new FormData();
+
+      if (editableSliderFile == null) {
+        formData.append("File", []);
+      } else {
+        for (let i = 0; i < editableSliderFile.length; i++) {
+          formData.append("File", editableSliderFile[i]);
+        }
+      }
+      formData.append("Id", editableSliderItemId);
+      formData.append("SliderId", editableSliderId);
+      formData.append("Title", editableSliderTitle);
+      formData.append("SubTitle", editableSliderSubtitle);
+      formData.append("ButtonText", editableSliderButtonText);
+      formData.append("ButtonLink", editableSliderButtonLink);
+
+      console.log(formData);
+      console.log(formData);
+      var object = {};
+      formData.forEach((value, key) => (object[key] = value));
+      var json = JSON.stringify(object);
+      console.log(json);
+
+      axios
+        .put(`http://api.mssdev.online/api/SliderItem`, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          console.log(response);
+          setInfo("Slider Başarıyla Düzenlendi");
+          setVariant("success");
+          setInfoModal(true);
+          setSliderChangeModal(false);
+          updateData();
+        })
+        .catch((error) => {
+          setInfo(error.response.data.Error);
+          setVariant("danger");
+          setInfoModal(true);
+        });
+    }
     return (
-      <div>
-        <p className="fw-semibold mb-3">2'li Slider Alanı</p>
-        <div className="mb-4">
-          <Table responsive hover borderless>
-            <thead>
-              <tr>
-                <th className="px-0">Sıra</th>
-                <th>Görsel</th>
-                <th>Başlık</th>
-                <th>Alt Başlık</th>
-                <th>Denetim</th>
-              </tr>
-            </thead>
-            <tbody>
-              {twoBanner.sliderItems &&
-                twoBanner.sliderItems.map((item, index) => (
-                  <tr key={index} className="align-middle">
-                    <td>{index + 1}</td>
-                    <td>
-                      <img
-                        height="50px"
-                        src={`http://api.mssdev.online/api/files/${item.imageUrl}`}
-                        alt={item.name}
-                      />
-                    </td>
-                    <td>{item.title}</td>
-                    <td>{item.subTitle}</td>
-                    <td>
-                      <button
-                        value={item.id}
-                        // onClick={(e) => deleteSliderItem(e.target.value)}
-                        className="btn btn-light border px-4 rounded-3"
-                      >
-                        Düzenle
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
-        </div>
-      </div>
-    );
-  }
-  function ThreeBanner() {
-    return (
-      <div>
-        <p className="fw-semibold mb-3">3'lü Banner Alanı</p>
-        <div className="mb-4">
-          <Table responsive hover borderless>
-            <thead>
-              <tr>
-                <th className="px-0">Sıra</th>
-                <th>Görsel</th>
-                <th>Başlık</th>
-                <th>Alt Başlık</th>
-                <th>Denetim</th>
-              </tr>
-            </thead>
-            <tbody>
-              {threeBanner.sliderItems &&
-                threeBanner.sliderItems.map((item, index) => (
-                  <tr key={index} className="align-middle">
-                    <td>{index + 1}</td>
-                    <td>
-                      <img
-                        height="50px"
-                        src={`http://api.mssdev.online/api/files/${item.imageUrl}`}
-                        alt={item.name}
-                      />
-                    </td>
-                    <td>{item.title}</td>
-                    <td>{item.subTitle}</td>
-                    <td>
-                      <button
-                        value={item.id}
-                        // onClick={(e) => deleteSliderItem(e.target.value)}
-                        className="btn btn-light border px-4 rounded-3"
-                      >
-                        Düzenle
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
-        </div>
-      </div>
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Slider Düzenle
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className="d-flex flex-column flex-column">
+            <label className="mb-3 fw-semibold ">Slider Başlığı</label>
+            <input
+              className="mb-3 py-2 px-3 text-muted bg-light rounded-3 shadow-sm border-0"
+              name="title"
+              type="text"
+              placeholder={editableSlider && editableSlider.title}
+              onChange={(e) => setEditableSliderTitle(e.target.value)}
+            />
+            <label className="mb-3 fw-semibold ">Slider Alt Başlığı</label>
+            <input
+              className="mb-3 p-3 text-muted bg-light rounded-3 shadow-sm border-0"
+              name="brand"
+              type="text"
+              placeholder={editableSlider && editableSlider.subTitle}
+              onChange={(e) => setEditableSliderSubtitle(e.target.value)}
+            />
+            <label className="mb-3 fw-semibold ">Slider Buton Yazısı</label>
+            <input
+              className="mb-3 py-2 px-3 text-muted bg-light rounded-3 shadow-sm border-0"
+              name="buttonText"
+              type="text"
+              placeholder={editableSlider && editableSlider.buttonText}
+              onChange={(e) => setEditableSliderButtonText(e.target.value)}
+            />
+            <label className="mb-3 fw-semibold ">Slider Buton Linki</label>
+            <input
+              className="mb-3 py-2 px-3 text-muted bg-light rounded-3 shadow-sm border-0"
+              name="buttonLink"
+              type="text"
+              placeholder={editableSlider && editableSlider.buttonLink}
+              onChange={(e) => setEditableSliderButtonLink(e.target.value)}
+            />
+            <label className="mb-3 fw-semibold ">Slider Görseli</label>
+            <div className="row">
+              <div className="image-edit col-2 d-flex align-items-center my-2">
+                <img
+                  width={"150px"}
+                  src={
+                    editableSlider.imageUrl &&
+                    `http://api.mssdev.online/api/files/${editableSlider.imageUrl}`
+                  }
+                  alt=""
+                />
+              </div>
+            </div>
+            <label className="mb-3 fw-semibold ">Görseli Değiştir</label>
+            <div className="file-input mb-3">
+              <input
+                className="product-image-button btn ps-0 w-100 border rounded-3"
+                type="file"
+                name="files"
+                id="files"
+                onChange={handleFile}
+                multiple
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-light border py-2 mb-2 rounded-3"
+              onClick={handleSubmit}
+            >
+              Düzenlemeleri Kaydet
+            </button>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <button
+            className="btn btn-light border rounded-3 px-5"
+            onClick={props.onHide}
+          >
+            Kapat
+          </button>
+        </Modal.Footer>
+      </Modal>
     );
   }
   function CarouselSlider() {
@@ -152,7 +246,7 @@ export default function AdminContents() {
         </div>
 
         <div className="mb-4">
-          <Table responsive hover borderless>
+          <Table responsive hover borderless variant="light">
             <thead>
               <tr>
                 <th className="px-0">Sıra</th>
@@ -179,8 +273,18 @@ export default function AdminContents() {
                     <td>
                       <button
                         value={item.id}
-                        onClick={(e) => deleteSliderItem(e.target.value)}
+                        onClick={(e) => {
+                          setSliderChangeModal(true);
+                          setChangeSliderItemId(e.target.value);
+                        }}
                         className="btn btn-light border px-4 rounded-3"
+                      >
+                        Düzenle
+                      </button>
+                      <button
+                        value={item.id}
+                        onClick={(e) => deleteSliderItem(e.target.value)}
+                        className="btn btn-light border px-4 rounded-3 ms-2"
                       >
                         Kaldır
                       </button>
@@ -193,6 +297,125 @@ export default function AdminContents() {
       </div>
     );
   }
+  function TwoBanner() {
+    return (
+      <div>
+        <p className="fw-semibold mb-3">2'li Slider Alanı</p>
+        <p>
+          <span className="fw-semibold">**İlk görsel:</span> Sol kısımda yer
+          alacak büyük görsel
+        </p>
+        <p>
+          <span className="fw-semibold">**İkinci görsel:</span> Sağ kısımda yer
+          alacak küçük görsel
+        </p>
+        <div className="mb-4">
+          <Table responsive hover borderless variant="light">
+            <thead>
+              <tr>
+                <th className="px-0">Sıra</th>
+                <th>Görsel</th>
+                <th>Başlık</th>
+                <th>Alt Başlık</th>
+                <th>Denetim</th>
+              </tr>
+            </thead>
+            <tbody>
+              {twoBanner.sliderItems &&
+                twoBanner.sliderItems.map((item, index) => (
+                  <tr key={index} className="align-middle">
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        height="50px"
+                        src={`http://api.mssdev.online/api/files/${item.imageUrl}`}
+                        alt={item.name}
+                      />
+                    </td>
+                    <td>{item.title}</td>
+                    <td>{item.subTitle}</td>
+                    <td>
+                      <button
+                        value={item.id}
+                        onClick={(e) => {
+                          setSliderChangeModal(true);
+                          setChangeSliderItemId(e.target.value);
+                        }}
+                        className="btn btn-light border px-4 rounded-3"
+                      >
+                        Düzenle
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    );
+  }
+  function ThreeBanner() {
+    return (
+      <div>
+        <p className="fw-semibold mb-3">3'lü Banner Alanı</p>
+        <p>
+          <span className="fw-semibold">**İlk görsel:</span> Sol kısımda yer
+          alacak büyük görsel
+        </p>
+        <p>
+          <span className="fw-semibold">**İkinci görsel:</span> Sağ üst kısımda
+          yer alacak küçük görsel
+        </p>
+        <p>
+          <span className="fw-semibold">**Üçünü görsel:</span> Sağ alt kısımda
+          yer alacak küçük görsel
+        </p>
+        <div className="mb-4">
+          <Table responsive hover borderless variant="light">
+            <thead>
+              <tr>
+                <th className="px-0">Sıra</th>
+                <th>Görsel</th>
+                <th>Başlık</th>
+                <th>Alt Başlık</th>
+                <th>Denetim</th>
+              </tr>
+            </thead>
+            <tbody>
+              {threeBanner.sliderItems &&
+                threeBanner.sliderItems.map((item, index) => (
+                  <tr key={index} className="align-middle">
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        height="50px"
+                        src={`http://api.mssdev.online/api/files/${item.imageUrl}`}
+                        alt={item.name}
+                      />
+                    </td>
+                    <td>{item.title}</td>
+                    <td>{item.subTitle}</td>
+                    <td>
+                      <button
+                        value={item.id}
+                        onClick={(e) => {
+                          setSliderChangeModal(true);
+                          setChangeSliderItemId(e.target.value);
+                        }}
+                        className="btn btn-light border px-4 rounded-3"
+                      >
+                        Düzenle
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
+        </div>
+      </div>
+    );
+  }
+
   function AddSliderItem(props) {
     //Post Stateleri
     const [sliderTitle, setSliderTitle] = useState("");
@@ -227,9 +450,7 @@ export default function AdminContents() {
           setInfo("Slider Başarıyla Eklendi");
           setVariant("success");
           setInfoModal(true);
-          setTimeout(() => {
-            window.location.reload(false);
-          }, 2000);
+          updateData();
         })
         .catch((error) => {
           setInfo(error.response.data.Error);
@@ -357,6 +578,10 @@ export default function AdminContents() {
         <AddSliderItem
           show={sliderAddModal}
           onHide={() => setSliderAddModal(false)}
+        />
+        <ChangeSliderItemModal
+          show={sliderChangeModal}
+          onHide={() => setSliderChangeModal(false)}
         />
       </div>
     </>
--- E-Commerce.React/src/components/admin/AdminHome.jsx
+++ E-Commerce.React/src/components/admin/AdminHome.jsx
@@ -4,6 +4,46 @@ export default function AdminHome() {
       <div className="row">
         <div className="panel col-12">
           <p className="mb-4 fs-4 fw-semibold text-muted">Yönetim Paneli</p>
+          <div className="row d-flex justify-content-between">
+            <div className="col-2 d-flex flex-column border rounded-3 shadow-sm">
+              <p className="text-success mt-3 text-end me-2">+%78</p>
+              <div className="d-flex flex-column align-items-center ">
+                <span className="fs-1 fw-semibold">
+                  24.000 <span className="fs-5">TRY</span>
+                </span>
+                <p className="mb-5 fs-5">Aylık Kazanç</p>
+              </div>
+            </div>
+            <div className="col-2 d-flex flex-column border rounded-3 shadow-sm">
+              <p className="text-success mt-3 text-end me-2">+%6</p>
+              <div className="d-flex flex-column align-items-center ">
+                <span className="fs-1 fw-semibold">80</span>
+                <p className="mb-5 fs-5">Yeni Sipariş</p>
+              </div>
+            </div>
+            <div className="col-2 d-flex flex-column border rounded-3 shadow-sm">
+              <p className="text-success mt-3 text-end me-2">+%10</p>
+              <div className="d-flex flex-column align-items-center ">
+                <span className="fs-1 fw-semibold">22</span>
+                <p className="mb-5 fs-5">Yeni Müşteri</p>
+              </div>
+            </div>
+            <div className="col-2 d-flex flex-column border rounded-3 shadow-sm">
+              <p className="text-success mt-3 text-end me-2">+%50</p>
+              <div className="d-flex flex-column align-items-center ">
+                <span className="fs-1 fw-semibold">40</span>
+                <p className="mb-5 fs-5">Yeni Ürün</p>
+              </div>
+            </div>
+            <div className="col-2 d-flex flex-column border rounded-3 shadow-sm">
+              <p className="text-success mt-3 text-end me-2">+%2</p>
+              <div className="d-flex flex-column align-items-center ">
+                <span className="fs-1 fw-semibold">5</span>
+                <p className="mb-5 fs-5">Yeni Yorum</p>
+              </div>
+            </div>
+          </div>
+          <div className="row"></div>
         </div>
       </div>
     </>
